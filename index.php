88d810ad77f483da2e23ab3272e5c5474f696499
dhendiii <dhendiawandwiatma@gmail.com>
2013-11-15 08:45:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once '../../restler/restler.php';
-
+require_once '../../vendor/restler.php';
+use Luracast\Restler\Restler;
 
 $r = new Restler();
 $r->setSupportedFormats('JsonFormat', 'XmlFormat');
